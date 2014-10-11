380b07518d89b900881a575b7b5533a018d2da8b
Maximiliano Felice <maximilianofelice@gmail.com>
2014-10-11 03:47:54 -0300

    Added Hotels, Regimens, Room type(WIP) migration with several bugfixes and an auxiliary script for manual inconsistency checking

---
+++ data/BOBBY_TABLES_SCHEMA/Auxiliary Migration Script.sql
@@ -0,0 +1,42 @@
+---------------- HOTELES ----------------
+SELECT DISTINCT Hotel_Ciudad, Hotel_Calle, Hotel_Nro_Calle, Hotel_CantEstrella, Hotel_Recarga_Estrella INTO #Hotels FROM gd_esquema.Maestra;
+SELECT * FROM #Hotels;
+
+/* No hay inconsistencias en las direcciones de hoteles */
+SELECT Hotel_Ciudad, Hotel_Calle, Hotel_Nro_Calle, COUNT(*) Cantidad_De_Repeticiones FROM #Hotels
+GROUP BY Hotel_Ciudad, Hotel_Calle, Hotel_Nro_Calle;
+
+---------------- CLIENTES ----------------
+SELECT DISTINCT Cliente_Pasaporte_Nro, Cliente_Apellido, Cliente_Nombre, Cliente_Fecha_Nac, Cliente_Mail, Cliente_Dom_Calle, Cliente_Nro_Calle, Cliente_Piso, Cliente_Depto, Cliente_Nacionalidad INTO #Clientes FROM gd_esquema.Maestra;
+SELECT * FROM #Clientes;
+
+/* Se puede ver que hay clientes con el mismo numero de pasaporte repetidos */
+SELECT Cliente_Pasaporte_Nro, COUNT(Cliente_Pasaporte_Nro) Cantidad_De_Repeticiones FROM #Clientes 
+GROUP BY Cliente_Pasaporte_Nro
+HAVING COUNT(Cliente_Pasaporte_Nro) > 1;
+
+
+
+---------------- REGIMENES ---------------- /* No Problems */
+SELECT DISTINCT Regimen_Descripcion, Regimen_Precio INTO #Regimens FROM gd_esquema.Maestra;
+SELECT * FROM #Regimens;
+
+
+---------------- ROOM TYPES ---------------- /* No Problems */
+SELECT DISTINCT Habitacion_Tipo_Codigo, Habitacion_Tipo_Descripcion, Habitacion_Tipo_Porcentual INTO #Room_Type FROM gd_esquema.Maestra;
+SELECT * FROM #Room_Type;
+
+
+
+---------------- EXTRAS ---------------- /* NULL REGISTERED, MUST BE CLEANED */
+SELECT DISTINCT Consumible_Codigo, Consumible_Descripcion, Consumible_Precio INTO #Extras FROM gd_esquema.Maestra;
+SELECT * FROM #Extras;
+
+
+DROP TABLE #Hotels;
+DROP TABLE #Clientes;
+DROP TABLE #Regimens;
+DROP TABLE #Room_Type;
+DROP TABLE #Extras;
+
+SELECT * FROM gd_esquema.Maestra;
