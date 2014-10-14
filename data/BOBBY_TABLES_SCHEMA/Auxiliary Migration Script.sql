c28a190333588a11cde6fd02b8f3634bfb4b4d01
Maximiliano Felice <maximilianofelice@gmail.com>
2014-10-13 22:47:05 -0300

    Added Rooms Migration

--- data/BOBBY_TABLES_SCHEMA/Auxiliary Migration Script.sql
+++ data/BOBBY_TABLES_SCHEMA/Auxiliary Migration Script.sql
@@ -31,6 +31,28 @@ SELECT * FROM #Room_Type;
 ---------------- EXTRAS ---------------- /* NULL REGISTERED, MUST BE CLEANED */
 SELECT DISTINCT Consumible_Codigo, Consumible_Descripcion, Consumible_Precio INTO #Extras FROM gd_esquema.Maestra;
 SELECT * FROM #Extras;
+SELECT * FROM #Extras WHERE Consumible_Codigo IS NULL;
+
+
+---------------- ROOM LOCATION ----------------
+SELECT DISTINCT Habitacion_Frente  INTO #Room_Loc FROM gd_esquema.Maestra;
+SELECT * FROM #Room_Loc;
+
+
+---------------- ROOMS ----------------
+SELECT DISTINCT ma.Habitacion_Numero, ma.Habitacion_Piso, hot.id_hotel, rt.id_roomtype, rl.id_location INTO #Rooms
+FROM gd_esquema.Maestra ma	INNER JOIN [BOBBY_TABLES].HOTELS hot ON (ma.Hotel_Ciudad = hot.city AND
+																	ma.Hotel_Calle = hot.street AND
+																	ma.Hotel_Nro_Calle = hot.street_num)
+							INNER JOIN [BOBBY_TABLES].ROOM_TYPE rt ON (ma.Habitacion_Tipo_Codigo = rt.id_roomtype)
+							INNER JOIN [BOBBY_TABLES].ROOM_LOCATION rl ON (ma.Habitacion_Frente = rl.descr);
+SELECT * FROM #Rooms;
+
+/* No hay habitaciones duplicadas en un mismo hotel */ 
+SELECT COUNT(*) Cant_Habitaciones FROM #Rooms
+GROUP BY Habitacion_Numero, Habitacion_Piso, id_hotel
+HAVING COUNT(*) > 1; 
+
 
 
 DROP TABLE #Hotels;
@@ -38,5 +60,7 @@ DROP TABLE #Clientes;
 DROP TABLE #Regimens;
 DROP TABLE #Room_Type;
 DROP TABLE #Extras;
+DROP TABLE #Room_Loc;
+DROP TABLE #Rooms;
 
 SELECT * FROM gd_esquema.Maestra;
