8bff5dcab5dbbee14d6cfade58c9601c6a870302
ffusaro <flor_fusaro@hotmail.com>
2014-10-26 01:25:28 -0200

    Added Create Room Structure

--- src/FrbaHotel/FrbaHotel/ABM de Habitacion/CreateRoom.Designer.cs
+++ src/FrbaHotel/FrbaHotel/ABM de Habitacion/CreateRoom.Designer.cs
@@ -28,11 +28,191 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.labelHotel = new System.Windows.Forms.Label();
+            this.textBoxHotel = new System.Windows.Forms.TextBox();
+            this.labelNumber = new System.Windows.Forms.Label();
+            this.textBoxNumber = new System.Windows.Forms.TextBox();
+            this.labelFloor = new System.Windows.Forms.Label();
+            this.textBoxFloor = new System.Windows.Forms.TextBox();
+            this.labelTpye = new System.Windows.Forms.Label();
+            this.textBoxType = new System.Windows.Forms.TextBox();
+            this.labelLocation = new System.Windows.Forms.Label();
+            this.textBoxLocation = new System.Windows.Forms.TextBox();
+            this.labelDescription = new System.Windows.Forms.Label();
+            this.textBoxDescription = new System.Windows.Forms.TextBox();
+            this.buttonSave = new System.Windows.Forms.Button();
+            this.buttonClear = new System.Windows.Forms.Button();
+            this.groupBox1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.textBoxDescription);
+            this.groupBox1.Controls.Add(this.labelDescription);
+            this.groupBox1.Controls.Add(this.textBoxLocation);
+            this.groupBox1.Controls.Add(this.labelLocation);
+            this.groupBox1.Controls.Add(this.textBoxType);
+            this.groupBox1.Controls.Add(this.labelTpye);
+            this.groupBox1.Controls.Add(this.textBoxFloor);
+            this.groupBox1.Controls.Add(this.labelFloor);
+            this.groupBox1.Controls.Add(this.textBoxNumber);
+            this.groupBox1.Controls.Add(this.labelNumber);
+            this.groupBox1.Controls.Add(this.textBoxHotel);
+            this.groupBox1.Controls.Add(this.labelHotel);
+            this.groupBox1.Location = new System.Drawing.Point(12, 12);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(227, 218);
+            this.groupBox1.TabIndex = 0;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Please enter the room information";
+            // 
+            // labelHotel
+            // 
+            this.labelHotel.AutoSize = true;
+            this.labelHotel.Location = new System.Drawing.Point(6, 32);
+            this.labelHotel.Name = "labelHotel";
+            this.labelHotel.Size = new System.Drawing.Size(32, 13);
+            this.labelHotel.TabIndex = 0;
+            this.labelHotel.Text = "Hotel";
+            // 
+            // textBoxHotel
+            // 
+            this.textBoxHotel.Location = new System.Drawing.Point(106, 29);
+            this.textBoxHotel.Name = "textBoxHotel";
+            this.textBoxHotel.Size = new System.Drawing.Size(100, 20);
+            this.textBoxHotel.TabIndex = 1;
+            // 
+            // labelNumber
+            // 
+            this.labelNumber.AutoSize = true;
+            this.labelNumber.Location = new System.Drawing.Point(6, 68);
+            this.labelNumber.Name = "labelNumber";
+            this.labelNumber.Size = new System.Drawing.Size(75, 13);
+            this.labelNumber.TabIndex = 2;
+            this.labelNumber.Text = "Room Number";
+            // 
+            // textBoxNumber
+            // 
+            this.textBoxNumber.Location = new System.Drawing.Point(106, 65);
+            this.textBoxNumber.Name = "textBoxNumber";
+            this.textBoxNumber.Size = new System.Drawing.Size(100, 20);
+            this.textBoxNumber.TabIndex = 3;
+            // 
+            // labelFloor
+            // 
+            this.labelFloor.AutoSize = true;
+            this.labelFloor.Location = new System.Drawing.Point(6, 99);
+            this.labelFloor.Name = "labelFloor";
+            this.labelFloor.Size = new System.Drawing.Size(61, 13);
+            this.labelFloor.TabIndex = 4;
+            this.labelFloor.Text = "Room Floor";
+            // 
+            // textBoxFloor
+            // 
+            this.textBoxFloor.Location = new System.Drawing.Point(106, 96);
+            this.textBoxFloor.Name = "textBoxFloor";
+            this.textBoxFloor.Size = new System.Drawing.Size(100, 20);
+            this.textBoxFloor.TabIndex = 5;
+            // 
+            // labelTpye
+            // 
+            this.labelTpye.AutoSize = true;
+            this.labelTpye.Location = new System.Drawing.Point(6, 126);
+            this.labelTpye.Name = "labelTpye";
+            this.labelTpye.Size = new System.Drawing.Size(62, 13);
+            this.labelTpye.TabIndex = 6;
+            this.labelTpye.Text = "Room Type";
+            // 
+            // textBoxType
+            // 
+            this.textBoxType.Location = new System.Drawing.Point(106, 123);
+            this.textBoxType.Name = "textBoxType";
+            this.textBoxType.Size = new System.Drawing.Size(100, 20);
+            this.textBoxType.TabIndex = 7;
+            // 
+            // labelLocation
+            // 
+            this.labelLocation.AutoSize = true;
+            this.labelLocation.Location = new System.Drawing.Point(9, 156);
+            this.labelLocation.Name = "labelLocation";
+            this.labelLocation.Size = new System.Drawing.Size(48, 13);
+            this.labelLocation.TabIndex = 8;
+            this.labelLocation.Text = "Location";
+            // 
+            // textBoxLocation
+            // 
+            this.textBoxLocation.Location = new System.Drawing.Point(106, 156);
+            this.textBoxLocation.Name = "textBoxLocation";
+            this.textBoxLocation.Size = new System.Drawing.Size(100, 20);
+            this.textBoxLocation.TabIndex = 9;
+            // 
+            // labelDescription
+            // 
+            this.labelDescription.AutoSize = true;
+            this.labelDescription.Location = new System.Drawing.Point(9, 185);
+            this.labelDescription.Name = "labelDescription";
+            this.labelDescription.Size = new System.Drawing.Size(60, 13);
+            this.labelDescription.TabIndex = 10;
+            this.labelDescription.Text = "Description";
+            // 
+            // textBoxDescription
+            // 
+            this.textBoxDescription.Location = new System.Drawing.Point(106, 185);
+            this.textBoxDescription.Name = "textBoxDescription";
+            this.textBoxDescription.Size = new System.Drawing.Size(100, 20);
+            this.textBoxDescription.TabIndex = 11;
+            // 
+            // buttonSave
+            // 
+            this.buttonSave.Location = new System.Drawing.Point(164, 249);
+            this.buttonSave.Name = "buttonSave";
+            this.buttonSave.Size = new System.Drawing.Size(75, 23);
+            this.buttonSave.TabIndex = 1;
+            this.buttonSave.Text = "Save";
+            this.buttonSave.UseVisualStyleBackColor = true;
+            // 
+            // buttonClear
+            // 
+            this.buttonClear.Location = new System.Drawing.Point(12, 249);
+            this.buttonClear.Name = "buttonClear";
+            this.buttonClear.Size = new System.Drawing.Size(75, 23);
+            this.buttonClear.TabIndex = 2;
+            this.buttonClear.Text = "Clear";
+            this.buttonClear.UseVisualStyleBackColor = true;
+            // 
+            // CreateRoom
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.Text = "CreateRoom";
+            this.ClientSize = new System.Drawing.Size(249, 285);
+            this.Controls.Add(this.buttonClear);
+            this.Controls.Add(this.buttonSave);
+            this.Controls.Add(this.groupBox1);
+            this.Name = "CreateRoom";
+            this.Text = "Create Room";
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.ResumeLayout(false);
+
         }
 
         #endregion
+
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.TextBox textBoxFloor;
+        private System.Windows.Forms.Label labelFloor;
+        private System.Windows.Forms.TextBox textBoxNumber;
+        private System.Windows.Forms.Label labelNumber;
+        private System.Windows.Forms.TextBox textBoxHotel;
+        private System.Windows.Forms.Label labelHotel;
+        private System.Windows.Forms.TextBox textBoxDescription;
+        private System.Windows.Forms.Label labelDescription;
+        private System.Windows.Forms.TextBox textBoxLocation;
+        private System.Windows.Forms.Label labelLocation;
+        private System.Windows.Forms.TextBox textBoxType;
+        private System.Windows.Forms.Label labelTpye;
+        private System.Windows.Forms.Button buttonSave;
+        private System.Windows.Forms.Button buttonClear;
     }
 }
