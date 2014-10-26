ea3101637c3e2a769175a574da7cac8b4eb3fd41
ffusaro <flor_fusaro@hotmail.com>
2014-10-26 19:26:27 -0200

    Added Update Role Structure

--- src/FrbaHotel/FrbaHotel/ABM de Rol/UpdateRole.Designer.cs
+++ src/FrbaHotel/FrbaHotel/ABM de Rol/UpdateRole.Designer.cs
@@ -28,11 +28,128 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.buttonSearchAll = new System.Windows.Forms.Button();
+            this.buttonSearch = new System.Windows.Forms.Button();
+            this.buttonClear = new System.Windows.Forms.Button();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.textBoxFeature = new System.Windows.Forms.TextBox();
+            this.labelFeatures = new System.Windows.Forms.Label();
+            this.textBoxName = new System.Windows.Forms.TextBox();
+            this.labelName = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.groupBox1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Location = new System.Drawing.Point(13, 122);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.Size = new System.Drawing.Size(624, 150);
+            this.dataGridView1.TabIndex = 12;
+            // 
+            // buttonSearchAll
+            // 
+            this.buttonSearchAll.Location = new System.Drawing.Point(284, 81);
+            this.buttonSearchAll.Name = "buttonSearchAll";
+            this.buttonSearchAll.Size = new System.Drawing.Size(75, 23);
+            this.buttonSearchAll.TabIndex = 11;
+            this.buttonSearchAll.Text = "Search All";
+            this.buttonSearchAll.UseVisualStyleBackColor = true;
+            // 
+            // buttonSearch
+            // 
+            this.buttonSearch.Location = new System.Drawing.Point(562, 81);
+            this.buttonSearch.Name = "buttonSearch";
+            this.buttonSearch.Size = new System.Drawing.Size(75, 23);
+            this.buttonSearch.TabIndex = 10;
+            this.buttonSearch.Text = "Search";
+            this.buttonSearch.UseVisualStyleBackColor = true;
+            // 
+            // buttonClear
+            // 
+            this.buttonClear.Location = new System.Drawing.Point(12, 81);
+            this.buttonClear.Name = "buttonClear";
+            this.buttonClear.Size = new System.Drawing.Size(75, 23);
+            this.buttonClear.TabIndex = 9;
+            this.buttonClear.Text = "Clear";
+            this.buttonClear.UseVisualStyleBackColor = true;
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.textBoxFeature);
+            this.groupBox1.Controls.Add(this.labelFeatures);
+            this.groupBox1.Controls.Add(this.textBoxName);
+            this.groupBox1.Controls.Add(this.labelName);
+            this.groupBox1.Location = new System.Drawing.Point(12, 12);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(625, 63);
+            this.groupBox1.TabIndex = 8;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Search Filters";
+            // 
+            // textBoxFeature
+            // 
+            this.textBoxFeature.Location = new System.Drawing.Point(486, 23);
+            this.textBoxFeature.Name = "textBoxFeature";
+            this.textBoxFeature.Size = new System.Drawing.Size(120, 20);
+            this.textBoxFeature.TabIndex = 3;
+            // 
+            // labelFeatures
+            // 
+            this.labelFeatures.AutoSize = true;
+            this.labelFeatures.Location = new System.Drawing.Point(332, 23);
+            this.labelFeatures.Name = "labelFeatures";
+            this.labelFeatures.Size = new System.Drawing.Size(100, 13);
+            this.labelFeatures.TabIndex = 2;
+            this.labelFeatures.Text = "Designated Feature";
+            // 
+            // textBoxName
+            // 
+            this.textBoxName.Location = new System.Drawing.Point(154, 20);
+            this.textBoxName.Name = "textBoxName";
+            this.textBoxName.Size = new System.Drawing.Size(120, 20);
+            this.textBoxName.TabIndex = 1;
+            // 
+            // labelName
+            // 
+            this.labelName.AutoSize = true;
+            this.labelName.Location = new System.Drawing.Point(6, 23);
+            this.labelName.Name = "labelName";
+            this.labelName.Size = new System.Drawing.Size(60, 13);
+            this.labelName.TabIndex = 0;
+            this.labelName.Text = "Role Name";
+            // 
+            // UpdateRole
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.Text = "UpdateRole";
+            this.ClientSize = new System.Drawing.Size(650, 283);
+            this.Controls.Add(this.dataGridView1);
+            this.Controls.Add(this.buttonSearchAll);
+            this.Controls.Add(this.buttonSearch);
+            this.Controls.Add(this.buttonClear);
+            this.Controls.Add(this.groupBox1);
+            this.Name = "UpdateRole";
+            this.Text = "Update Role";
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.ResumeLayout(false);
+
         }
 
         #endregion
+
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.Button buttonSearchAll;
+        private System.Windows.Forms.Button buttonSearch;
+        private System.Windows.Forms.Button buttonClear;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.TextBox textBoxFeature;
+        private System.Windows.Forms.Label labelFeatures;
+        private System.Windows.Forms.TextBox textBoxName;
+        private System.Windows.Forms.Label labelName;
     }
 }
