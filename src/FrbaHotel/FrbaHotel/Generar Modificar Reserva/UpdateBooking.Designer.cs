356a55fb07112dc58474ea518f84b54c24dc0e4c
ffusaro <flor_fusaro@hotmail.com>
2014-10-26 21:08:01 -0200

    Added Update Booking Structure

--- src/FrbaHotel/FrbaHotel/Generar Modificar Reserva/UpdateBooking.Designer.cs
+++ src/FrbaHotel/FrbaHotel/Generar Modificar Reserva/UpdateBooking.Designer.cs
@@ -28,11 +28,242 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.textBoxExtraGuests = new System.Windows.Forms.TextBox();
+            this.labelDocNumber = new System.Windows.Forms.Label();
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.labelName = new System.Windows.Forms.Label();
+            this.textBoxDocNumber = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.labelRegimen = new System.Windows.Forms.Label();
+            this.comboBoxRegimen = new System.Windows.Forms.ComboBox();
+            this.labelCheckout = new System.Windows.Forms.Label();
+            this.labelCheckin = new System.Windows.Forms.Label();
+            this.labelHotel = new System.Windows.Forms.Label();
+            this.comboBoxHotel = new System.Windows.Forms.ComboBox();
+            this.textBoxCheckin = new System.Windows.Forms.TextBox();
+            this.textBoxCheckout = new System.Windows.Forms.TextBox();
+            this.buttonClear = new System.Windows.Forms.Button();
+            this.buttonSearchAll = new System.Windows.Forms.Button();
+            this.buttonSearch = new System.Windows.Forms.Button();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.groupBox1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.textBoxCheckout);
+            this.groupBox1.Controls.Add(this.textBoxCheckin);
+            this.groupBox1.Controls.Add(this.textBoxExtraGuests);
+            this.groupBox1.Controls.Add(this.labelDocNumber);
+            this.groupBox1.Controls.Add(this.comboBox1);
+            this.groupBox1.Controls.Add(this.labelName);
+            this.groupBox1.Controls.Add(this.textBoxDocNumber);
+            this.groupBox1.Controls.Add(this.label1);
+            this.groupBox1.Controls.Add(this.labelRegimen);
+            this.groupBox1.Controls.Add(this.comboBoxRegimen);
+            this.groupBox1.Controls.Add(this.labelCheckout);
+            this.groupBox1.Controls.Add(this.labelCheckin);
+            this.groupBox1.Controls.Add(this.labelHotel);
+            this.groupBox1.Controls.Add(this.comboBoxHotel);
+            this.groupBox1.Location = new System.Drawing.Point(12, 12);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(644, 144);
+            this.groupBox1.TabIndex = 1;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Search Filters";
+            // 
+            // textBoxExtraGuests
+            // 
+            this.textBoxExtraGuests.Location = new System.Drawing.Point(187, 86);
+            this.textBoxExtraGuests.Name = "textBoxExtraGuests";
+            this.textBoxExtraGuests.Size = new System.Drawing.Size(100, 20);
+            this.textBoxExtraGuests.TabIndex = 13;
+            // 
+            // labelDocNumber
+            // 
+            this.labelDocNumber.AutoSize = true;
+            this.labelDocNumber.Location = new System.Drawing.Point(361, 62);
+            this.labelDocNumber.Name = "labelDocNumber";
+            this.labelDocNumber.Size = new System.Drawing.Size(153, 13);
+            this.labelDocNumber.TabIndex = 12;
+            this.labelDocNumber.Text = "Booking Holder\'s Doc. Number";
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Location = new System.Drawing.Point(187, 59);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(100, 21);
+            this.comboBox1.TabIndex = 11;
+            // 
+            // labelName
+            // 
+            this.labelName.AutoSize = true;
+            this.labelName.Location = new System.Drawing.Point(15, 62);
+            this.labelName.Name = "labelName";
+            this.labelName.Size = new System.Drawing.Size(140, 13);
+            this.labelName.TabIndex = 10;
+            this.labelName.Text = "Booking Holder\'s Doc. Type";
+            // 
+            // textBoxDocNumber
+            // 
+            this.textBoxDocNumber.Location = new System.Drawing.Point(532, 59);
+            this.textBoxDocNumber.Name = "textBoxDocNumber";
+            this.textBoxDocNumber.Size = new System.Drawing.Size(100, 20);
+            this.textBoxDocNumber.TabIndex = 9;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(15, 89);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(67, 13);
+            this.label1.TabIndex = 8;
+            this.label1.Text = "Extra Guests";
+            // 
+            // labelRegimen
+            // 
+            this.labelRegimen.AutoSize = true;
+            this.labelRegimen.Location = new System.Drawing.Point(361, 35);
+            this.labelRegimen.Name = "labelRegimen";
+            this.labelRegimen.Size = new System.Drawing.Size(76, 13);
+            this.labelRegimen.TabIndex = 7;
+            this.labelRegimen.Text = "Regimen Type";
+            // 
+            // comboBoxRegimen
+            // 
+            this.comboBoxRegimen.FormattingEnabled = true;
+            this.comboBoxRegimen.Location = new System.Drawing.Point(532, 32);
+            this.comboBoxRegimen.Name = "comboBoxRegimen";
+            this.comboBoxRegimen.Size = new System.Drawing.Size(100, 21);
+            this.comboBoxRegimen.TabIndex = 6;
+            // 
+            // labelCheckout
+            // 
+            this.labelCheckout.AutoSize = true;
+            this.labelCheckout.Location = new System.Drawing.Point(361, 89);
+            this.labelCheckout.Name = "labelCheckout";
+            this.labelCheckout.Size = new System.Drawing.Size(82, 13);
+            this.labelCheckout.TabIndex = 5;
+            this.labelCheckout.Text = "Check-out Date";
+            // 
+            // labelCheckin
+            // 
+            this.labelCheckin.AutoSize = true;
+            this.labelCheckin.Location = new System.Drawing.Point(15, 115);
+            this.labelCheckin.Name = "labelCheckin";
+            this.labelCheckin.Size = new System.Drawing.Size(75, 13);
+            this.labelCheckin.TabIndex = 3;
+            this.labelCheckin.Text = "Check-in Date";
+            // 
+            // labelHotel
+            // 
+            this.labelHotel.AutoSize = true;
+            this.labelHotel.Location = new System.Drawing.Point(15, 35);
+            this.labelHotel.Name = "labelHotel";
+            this.labelHotel.Size = new System.Drawing.Size(32, 13);
+            this.labelHotel.TabIndex = 1;
+            this.labelHotel.Text = "Hotel";
+            // 
+            // comboBoxHotel
+            // 
+            this.comboBoxHotel.FormattingEnabled = true;
+            this.comboBoxHotel.Location = new System.Drawing.Point(187, 32);
+            this.comboBoxHotel.Name = "comboBoxHotel";
+            this.comboBoxHotel.Size = new System.Drawing.Size(100, 21);
+            this.comboBoxHotel.TabIndex = 0;
+            // 
+            // textBoxCheckin
+            // 
+            this.textBoxCheckin.Location = new System.Drawing.Point(187, 112);
+            this.textBoxCheckin.Name = "textBoxCheckin";
+            this.textBoxCheckin.Size = new System.Drawing.Size(100, 20);
+            this.textBoxCheckin.TabIndex = 14;
+            // 
+            // textBoxCheckout
+            // 
+            this.textBoxCheckout.Location = new System.Drawing.Point(532, 86);
+            this.textBoxCheckout.Name = "textBoxCheckout";
+            this.textBoxCheckout.Size = new System.Drawing.Size(100, 20);
+            this.textBoxCheckout.TabIndex = 15;
+            // 
+            // buttonClear
+            // 
+            this.buttonClear.Location = new System.Drawing.Point(13, 163);
+            this.buttonClear.Name = "buttonClear";
+            this.buttonClear.Size = new System.Drawing.Size(75, 23);
+            this.buttonClear.TabIndex = 2;
+            this.buttonClear.Text = "Clear";
+            this.buttonClear.UseVisualStyleBackColor = true;
+            // 
+            // buttonSearchAll
+            // 
+            this.buttonSearchAll.Location = new System.Drawing.Point(281, 162);
+            this.buttonSearchAll.Name = "buttonSearchAll";
+            this.buttonSearchAll.Size = new System.Drawing.Size(75, 23);
+            this.buttonSearchAll.TabIndex = 3;
+            this.buttonSearchAll.Text = "Search All";
+            this.buttonSearchAll.UseVisualStyleBackColor = true;
+            // 
+            // buttonSearch
+            // 
+            this.buttonSearch.Location = new System.Drawing.Point(568, 162);
+            this.buttonSearch.Name = "buttonSearch";
+            this.buttonSearch.Size = new System.Drawing.Size(75, 23);
+            this.buttonSearch.TabIndex = 4;
+            this.buttonSearch.Text = "Search";
+            this.buttonSearch.UseVisualStyleBackColor = true;
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Location = new System.Drawing.Point(13, 193);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.Size = new System.Drawing.Size(643, 150);
+            this.dataGridView1.TabIndex = 5;
+            // 
+            // UpdateBooking
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.Text = "UpdateBooking";
+            this.ClientSize = new System.Drawing.Size(674, 370);
+            this.Controls.Add(this.dataGridView1);
+            this.Controls.Add(this.buttonSearch);
+            this.Controls.Add(this.buttonSearchAll);
+            this.Controls.Add(this.buttonClear);
+            this.Controls.Add(this.groupBox1);
+            this.Name = "UpdateBooking";
+            this.Text = "Update Booking";
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.ResumeLayout(false);
+
         }
 
         #endregion
+
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.TextBox textBoxCheckout;
+        private System.Windows.Forms.TextBox textBoxCheckin;
+        private System.Windows.Forms.TextBox textBoxExtraGuests;
+        private System.Windows.Forms.Label labelDocNumber;
+        private System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.Label labelName;
+        private System.Windows.Forms.TextBox textBoxDocNumber;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label labelRegimen;
+        private System.Windows.Forms.ComboBox comboBoxRegimen;
+        private System.Windows.Forms.Label labelCheckout;
+        private System.Windows.Forms.Label labelCheckin;
+        private System.Windows.Forms.Label labelHotel;
+        private System.Windows.Forms.ComboBox comboBoxHotel;
+        private System.Windows.Forms.Button buttonClear;
+        private System.Windows.Forms.Button buttonSearchAll;
+        private System.Windows.Forms.Button buttonSearch;
+        private System.Windows.Forms.DataGridView dataGridView1;
+
     }
 }
