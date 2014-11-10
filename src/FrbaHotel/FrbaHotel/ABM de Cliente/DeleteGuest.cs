25a3201880b0fe9669d1d60c1515119405cd5ba6
ffusaro <flor_fusaro@hotmail.com>
2014-11-10 12:37:47 -0200

    Base filter SearchAll DeleteGuest

--- src/FrbaHotel/FrbaHotel/ABM de Cliente/DeleteGuest.cs
+++ src/FrbaHotel/FrbaHotel/ABM de Cliente/DeleteGuest.cs
@@ -6,14 +6,166 @@ using System.Drawing;
 using System.Linq;
 using System.Text;
 using System.Windows.Forms;
+using HotelModel.Home;
+using HotelModel.DB_Conn_DSL;
 
 namespace FrbaHotel.ABM_de_Cliente
 {
     public partial class DeleteGuest : Form
     {
+        String name { get; set; }
+        String lastname { get; set; }
+        String docType { get; set; }
+        Decimal docNumber { get; set; }
+        String mail { get; set; }
+        Decimal phone { get; set; }
+        DateTime birthDate { get; set; }
+        String street { get; set; }
+        Int32 streetNum { get; set; }
+        Int32 floor { get; set; }
+        String dept { get; set; }
+        String nationality { get; set; }
+        Int32 state { get; set; }
+
+
         public DeleteGuest()
         {
             InitializeComponent();
         }
+
+
+
+        private void buttonClear_Click(object sender, EventArgs e)
+        {
+            textBoxName.Clear();
+            textBoxLastname.Clear();
+            comboBoxDocType.Text = "";
+            textBoxDocNumber.Clear();
+            textBoxEmail.Clear();
+            textBoxPhone.Clear();
+            textBoxBirthDate.Clear();
+            textBoxStreet.Clear();
+            textBoxDept.Clear();
+            textBoxFloor.Clear();
+            textBoxStreetNum.Clear();
+            textBoxDept.Clear();
+            textBoxNationality.Clear();
+        }
+
+        private void buttonSearchAll_Click(object sender, EventArgs e)
+        {
+
+            SqlResults results = new SqlQuery("SELECT * FROM [BOBBY_TABLES].PERSONS;").AsDataTable().Execute();
+
+            DataTable ReturnedTable = (DataTable)results["ReturnedValues"];
+
+            dataGridView1.DataSource = ReturnedTable;
+
+
+        }
+
+        private void buttonSearch_Click(object sender, EventArgs e)
+        {
+
+            this.validateInput();
+            GuestHandler searchGuests = new GuestHandler();
+
+            dataGridView1.DataSource = searchGuests.filteredSearch(name, lastname, docType,
+                                          docNumber, mail, phone, birthDate, street,
+                                          streetNum, floor, dept, nationality, state);
+        }
+
+        private void dataGridView1_CellContentClick(object sender, DataGridViewCellEventArgs e)
+        {
+
+        }
+
+
+
+
+        public bool validateInput()
+        {
+         
+            name = textBoxName.Text;
+            lastname = textBoxLastname.Text;
+            docType = comboBoxDocType.Text;
+          
+            //validates decimal type in docnumber
+            if(!String.IsNullOrEmpty(textBoxDocNumber.Text)){
+                try
+                {
+                     docNumber = Decimal.Parse(textBoxDocNumber.Text);
+                }
+                catch (FormatException)
+                {
+                
+                    MessageBox.Show("Document Number has an invalid type");
+                }
+
+            }else{
+                    docNumber= -1;
+            }
+
+            //validates decimal type in phone
+            if(!String.IsNullOrEmpty(textBoxPhone.Text))
+            {
+                try
+                 {
+                     phone = Decimal.Parse(textBoxPhone.Text); ;
+
+                  }
+                 catch (FormatException)
+                 {
+               
+                     MessageBox.Show("Phone Number has an invalid type");
+                  }
+            }else{
+                phone=-1;
+            }
+
+
+
+
+            //validates datetime type in birthdate
+            if(!String.IsNullOrEmpty(textBoxBirthDate.Text))
+            {
+                try
+                {
+                     birthDate = DateTime.Parse(textBoxBirthDate.Text);
+                }
+                catch (FormatException)
+                {
+                   MessageBox.Show("Birth Date has an invalid type");
+                }
+            }
+
+
+
+
+            //validates int type in street num
+            try
+            {
+                streetNum = Int32.Parse(textBoxStreetNum.Text);
+            }
+            catch (FormatException)
+            {
+                
+                MessageBox.Show("Street Num has an invalid type");
+            }
+
+            //validates int type in floor
+            try
+            {
+                floor = Int32.Parse(textBoxFloor.Text);
+            }
+            catch (FormatException)
+            {
+                
+                MessageBox.Show("Street Num has an invalid type");
+            }
+
+            return true;
+
+    }
     }
 }
--- src/FrbaHotel/HotelModel/Home/GuestHandler.cs
+++ src/FrbaHotel/HotelModel/Home/GuestHandler.cs
@@ -63,7 +63,7 @@ namespace HotelModel.Home
                                 String street, Int32 streetNum,Int32 floor, String dept, String nationality,Int32 state)
         {
 
-            SqlResults results = new SqlStoredProcedure("[BOBBY_TABLES].updatePerson")
+            SqlResults results = new SqlStoredProcedure("[BOBBY_TABLES].UpdatePerson")
                                      .WithParam("@Name").As(SqlDbType.VarChar).Value(name)
                                      .WithParam("@Lastname").As(SqlDbType.VarChar).Value(lastname)
                                      .WithParam("@DocType").As(SqlDbType.VarChar).Value(docType)
@@ -76,10 +76,37 @@ namespace HotelModel.Home
                                      .WithParam("@Floor").As(SqlDbType.Int).Value(floor)
                                      .WithParam("@Dept").As(SqlDbType.VarChar).Value(dept)
                                      .WithParam("@Nationality").As(SqlDbType.VarChar).Value(nationality)
+                                     .WithParam("@State").As(SqlDbType.Int).Value(state)
                                      .WithParam("@IdPersonUpdated").As(SqlDbType.Int).AsOutput()
                                                              .Execute();
 
             return (Int32)results["@IdPersonUpdated"];
+        }
+
+
+        public DataSet filteredSearch(String name, String lastname, String docType, Decimal docNumber, String mail, Decimal phone, DateTime birthDate,
+                                String street, Int32 streetNum, Int32 floor, String dept, String nationality, Int32 state) {
+
+
+                                    SqlResults results = new SqlStoredProcedure("[BOBBY_TABLES].filteredPerson")
+                                                             .WithParam("@Name").As(SqlDbType.VarChar).Value(name)
+                                                             .WithParam("@Lastname").As(SqlDbType.VarChar).Value(lastname)
+                                                             .WithParam("@DocType").As(SqlDbType.VarChar).Value(docType)
+                                                             .WithParam("@DocNumber").As(SqlDbType.Decimal).Value(docNumber)
+                                                             .WithParam("@Mail").As(SqlDbType.VarChar).Value(mail)
+                                                             .WithParam("@Phone").As(SqlDbType.Decimal).Value(phone)
+                                                             .WithParam("@BirthDate").As(SqlDbType.DateTime).Value(birthDate)
+                                                             .WithParam("@Street").As(SqlDbType.VarChar).Value(street)
+                                                             .WithParam("@StreetNum").As(SqlDbType.Int).Value(streetNum)
+                                                             .WithParam("@Floor").As(SqlDbType.Int).Value(floor)
+                                                             .WithParam("@Dept").As(SqlDbType.VarChar).Value(dept)
+                                                             .WithParam("@Nationality").As(SqlDbType.VarChar).Value(nationality)
+                                                             .WithParam("@ReturnedValues").AsDataSet().AsOutput()
+                                                                                     .Execute();
+
+
+            return (DataSet) results["ReturnedValues"];
+        
         }
 
 
