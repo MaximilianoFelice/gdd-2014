cbec93d2635de4cbf5907512a355b134dfc8bbe2
ffusaro <flor_fusaro@hotmail.com>
2014-11-10 11:50:06 -0200

    Added street, streetnum, floor and dept in delete guest form

--- src/FrbaHotel/FrbaHotel/ABM de Cliente/DeleteGuest.Designer.cs
+++ src/FrbaHotel/FrbaHotel/ABM de Cliente/DeleteGuest.Designer.cs
@@ -29,8 +29,15 @@
         private void InitializeComponent()
         {
             this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.textBoxDept = new System.Windows.Forms.TextBox();
+            this.textBoxFloor = new System.Windows.Forms.TextBox();
+            this.textBoxStreetNum = new System.Windows.Forms.TextBox();
+            this.textBoxStreet = new System.Windows.Forms.TextBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
             this.textBoxNationality = new System.Windows.Forms.TextBox();
-            this.textBoxAddress = new System.Windows.Forms.TextBox();
             this.textBoxBirthDate = new System.Windows.Forms.TextBox();
             this.textBoxPhone = new System.Windows.Forms.TextBox();
             this.textBoxEmail = new System.Windows.Forms.TextBox();
@@ -39,7 +46,6 @@
             this.textBoxLastname = new System.Windows.Forms.TextBox();
             this.textBoxName = new System.Windows.Forms.TextBox();
             this.labelNationality = new System.Windows.Forms.Label();
-            this.labelAddress = new System.Windows.Forms.Label();
             this.labelBirthDate = new System.Windows.Forms.Label();
             this.labelPhone = new System.Windows.Forms.Label();
             this.labelEmail = new System.Windows.Forms.Label();
@@ -51,14 +57,22 @@
             this.buttonSearchAll = new System.Windows.Forms.Button();
             this.buttonSearch = new System.Windows.Forms.Button();
             this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.DeleteCheck = new System.Windows.Forms.DataGridViewCheckBoxColumn();
             this.groupBox1.SuspendLayout();
             ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
             this.SuspendLayout();
             // 
             // groupBox1
             // 
+            this.groupBox1.Controls.Add(this.textBoxDept);
+            this.groupBox1.Controls.Add(this.textBoxFloor);
+            this.groupBox1.Controls.Add(this.textBoxStreetNum);
+            this.groupBox1.Controls.Add(this.textBoxStreet);
+            this.groupBox1.Controls.Add(this.label4);
+            this.groupBox1.Controls.Add(this.label3);
+            this.groupBox1.Controls.Add(this.label2);
+            this.groupBox1.Controls.Add(this.label1);
             this.groupBox1.Controls.Add(this.textBoxNationality);
-            this.groupBox1.Controls.Add(this.textBoxAddress);
             this.groupBox1.Controls.Add(this.textBoxBirthDate);
             this.groupBox1.Controls.Add(this.textBoxPhone);
             this.groupBox1.Controls.Add(this.textBoxEmail);
@@ -67,7 +81,6 @@
             this.groupBox1.Controls.Add(this.textBoxLastname);
             this.groupBox1.Controls.Add(this.textBoxName);
             this.groupBox1.Controls.Add(this.labelNationality);
-            this.groupBox1.Controls.Add(this.labelAddress);
             this.groupBox1.Controls.Add(this.labelBirthDate);
             this.groupBox1.Controls.Add(this.labelPhone);
             this.groupBox1.Controls.Add(this.labelEmail);
@@ -77,42 +90,99 @@
             this.groupBox1.Controls.Add(this.labelName);
             this.groupBox1.Location = new System.Drawing.Point(13, 13);
             this.groupBox1.Name = "groupBox1";
-            this.groupBox1.Size = new System.Drawing.Size(643, 168);
+            this.groupBox1.Size = new System.Drawing.Size(857, 168);
             this.groupBox1.TabIndex = 0;
             this.groupBox1.TabStop = false;
             this.groupBox1.Text = "Search Filters";
             // 
+            // textBoxDept
+            // 
+            this.textBoxDept.Location = new System.Drawing.Point(743, 107);
+            this.textBoxDept.Name = "textBoxDept";
+            this.textBoxDept.Size = new System.Drawing.Size(100, 20);
+            this.textBoxDept.TabIndex = 41;
+            // 
+            // textBoxFloor
+            // 
+            this.textBoxFloor.Location = new System.Drawing.Point(743, 81);
+            this.textBoxFloor.Name = "textBoxFloor";
+            this.textBoxFloor.Size = new System.Drawing.Size(100, 20);
+            this.textBoxFloor.TabIndex = 40;
+            // 
+            // textBoxStreetNum
+            // 
+            this.textBoxStreetNum.Location = new System.Drawing.Point(743, 55);
+            this.textBoxStreetNum.Name = "textBoxStreetNum";
+            this.textBoxStreetNum.Size = new System.Drawing.Size(100, 20);
+            this.textBoxStreetNum.TabIndex = 39;
+            // 
+            // textBoxStreet
+            // 
+            this.textBoxStreet.Location = new System.Drawing.Point(743, 28);
+            this.textBoxStreet.Name = "textBoxStreet";
+            this.textBoxStreet.Size = new System.Drawing.Size(100, 20);
+            this.textBoxStreet.TabIndex = 38;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(647, 110);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(62, 13);
+            this.label4.TabIndex = 37;
+            this.label4.Text = "Department";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(647, 83);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(30, 13);
+            this.label3.TabIndex = 36;
+            this.label3.Text = "Floor";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(647, 54);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(75, 13);
+            this.label2.TabIndex = 35;
+            this.label2.Text = "Street Number";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(647, 31);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(35, 13);
+            this.label1.TabIndex = 34;
+            this.label1.Text = "Street";
+            // 
             // textBoxNationality
             // 
-            this.textBoxNationality.Location = new System.Drawing.Point(465, 107);
+            this.textBoxNationality.Location = new System.Drawing.Point(448, 107);
             this.textBoxNationality.Name = "textBoxNationality";
             this.textBoxNationality.Size = new System.Drawing.Size(137, 20);
             this.textBoxNationality.TabIndex = 20;
             // 
-            // textBoxAddress
-            // 
-            this.textBoxAddress.Location = new System.Drawing.Point(465, 81);
-            this.textBoxAddress.Name = "textBoxAddress";
-            this.textBoxAddress.Size = new System.Drawing.Size(137, 20);
-            this.textBoxAddress.TabIndex = 34;
-            // 
             // textBoxBirthDate
             // 
-            this.textBoxBirthDate.Location = new System.Drawing.Point(465, 54);
+            this.textBoxBirthDate.Location = new System.Drawing.Point(448, 54);
             this.textBoxBirthDate.Name = "textBoxBirthDate";
             this.textBoxBirthDate.Size = new System.Drawing.Size(137, 20);
             this.textBoxBirthDate.TabIndex = 33;
             // 
             // textBoxPhone
             // 
-            this.textBoxPhone.Location = new System.Drawing.Point(465, 28);
+            this.textBoxPhone.Location = new System.Drawing.Point(448, 28);
             this.textBoxPhone.Name = "textBoxPhone";
             this.textBoxPhone.Size = new System.Drawing.Size(137, 20);
             this.textBoxPhone.TabIndex = 32;
             // 
             // textBoxEmail
             // 
-            this.textBoxEmail.Location = new System.Drawing.Point(143, 133);
+            this.textBoxEmail.Location = new System.Drawing.Point(448, 80);
             this.textBoxEmail.Name = "textBoxEmail";
             this.textBoxEmail.Size = new System.Drawing.Size(137, 20);
             this.textBoxEmail.TabIndex = 31;
@@ -149,25 +219,16 @@
             // labelNationality
             // 
             this.labelNationality.AutoSize = true;
-            this.labelNationality.Location = new System.Drawing.Point(354, 110);
+            this.labelNationality.Location = new System.Drawing.Point(330, 110);
             this.labelNationality.Name = "labelNationality";
             this.labelNationality.Size = new System.Drawing.Size(56, 13);
             this.labelNationality.TabIndex = 26;
             this.labelNationality.Text = "Nationality";
             // 
-            // labelAddress
-            // 
-            this.labelAddress.AutoSize = true;
-            this.labelAddress.Location = new System.Drawing.Point(354, 83);
-            this.labelAddress.Name = "labelAddress";
-            this.labelAddress.Size = new System.Drawing.Size(45, 13);
-            this.labelAddress.TabIndex = 25;
-            this.labelAddress.Text = "Address";
-            // 
             // labelBirthDate
             // 
             this.labelBirthDate.AutoSize = true;
-            this.labelBirthDate.Location = new System.Drawing.Point(354, 57);
+            this.labelBirthDate.Location = new System.Drawing.Point(330, 54);
             this.labelBirthDate.Name = "labelBirthDate";
             this.labelBirthDate.Size = new System.Drawing.Size(54, 13);
             this.labelBirthDate.TabIndex = 24;
@@ -176,7 +237,7 @@
             // labelPhone
             // 
             this.labelPhone.AutoSize = true;
-            this.labelPhone.Location = new System.Drawing.Point(354, 31);
+            this.labelPhone.Location = new System.Drawing.Point(330, 31);
             this.labelPhone.Name = "labelPhone";
             this.labelPhone.Size = new System.Drawing.Size(78, 13);
             this.labelPhone.TabIndex = 23;
@@ -185,7 +246,7 @@
             // labelEmail
             // 
             this.labelEmail.AutoSize = true;
-            this.labelEmail.Location = new System.Drawing.Point(21, 136);
+            this.labelEmail.Location = new System.Drawing.Point(330, 83);
             this.labelEmail.Name = "labelEmail";
             this.labelEmail.Size = new System.Drawing.Size(35, 13);
             this.labelEmail.TabIndex = 22;
@@ -235,38 +296,49 @@
             this.buttonClear.TabIndex = 1;
             this.buttonClear.Text = "Clear";
             this.buttonClear.UseVisualStyleBackColor = true;
+            this.buttonClear.Click += new System.EventHandler(this.buttonClear_Click);
             // 
             // buttonSearchAll
             // 
-            this.buttonSearchAll.Location = new System.Drawing.Point(317, 187);
+            this.buttonSearchAll.Location = new System.Drawing.Point(398, 187);
             this.buttonSearchAll.Name = "buttonSearchAll";
             this.buttonSearchAll.Size = new System.Drawing.Size(75, 23);
             this.buttonSearchAll.TabIndex = 2;
             this.buttonSearchAll.Text = "Search All";
             this.buttonSearchAll.UseVisualStyleBackColor = true;
+            this.buttonSearchAll.Click += new System.EventHandler(this.buttonSearchAll_Click);
             // 
             // buttonSearch
             // 
-            this.buttonSearch.Location = new System.Drawing.Point(581, 187);
+            this.buttonSearch.Location = new System.Drawing.Point(795, 187);
             this.buttonSearch.Name = "buttonSearch";
             this.buttonSearch.Size = new System.Drawing.Size(75, 23);
             this.buttonSearch.TabIndex = 3;
             this.buttonSearch.Text = "Search";
             this.buttonSearch.UseVisualStyleBackColor = true;
+            this.buttonSearch.Click += new System.EventHandler(this.buttonSearch_Click);
             // 
             // dataGridView1
             // 
             this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.DeleteCheck});
             this.dataGridView1.Location = new System.Drawing.Point(13, 216);
             this.dataGridView1.Name = "dataGridView1";
-            this.dataGridView1.Size = new System.Drawing.Size(643, 150);
+            this.dataGridView1.Size = new System.Drawing.Size(857, 150);
             this.dataGridView1.TabIndex = 4;
+            this.dataGridView1.CellContentClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.dataGridView1_CellContentClick);
+            // 
+            // DeleteCheck
+            // 
+            this.DeleteCheck.HeaderText = "Select to Delete";
+            this.DeleteCheck.Name = "DeleteCheck";
             // 
             // DeleteGuest
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(666, 378);
+            this.ClientSize = new System.Drawing.Size(892, 378);
             this.Controls.Add(this.dataGridView1);
             this.Controls.Add(this.buttonSearch);
             this.Controls.Add(this.buttonSearchAll);
@@ -285,7 +357,6 @@
 
         private System.Windows.Forms.GroupBox groupBox1;
         private System.Windows.Forms.TextBox textBoxNationality;
-        private System.Windows.Forms.TextBox textBoxAddress;
         private System.Windows.Forms.TextBox textBoxBirthDate;
         private System.Windows.Forms.TextBox textBoxPhone;
         private System.Windows.Forms.TextBox textBoxEmail;
@@ -294,7 +365,6 @@
         private System.Windows.Forms.TextBox textBoxLastname;
         private System.Windows.Forms.TextBox textBoxName;
         private System.Windows.Forms.Label labelNationality;
-        private System.Windows.Forms.Label labelAddress;
         private System.Windows.Forms.Label labelBirthDate;
         private System.Windows.Forms.Label labelPhone;
         private System.Windows.Forms.Label labelEmail;
@@ -306,5 +376,14 @@
         private System.Windows.Forms.Button buttonSearchAll;
         private System.Windows.Forms.Button buttonSearch;
         private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox textBoxDept;
+        private System.Windows.Forms.TextBox textBoxFloor;
+        private System.Windows.Forms.TextBox textBoxStreetNum;
+        private System.Windows.Forms.TextBox textBoxStreet;
+        private System.Windows.Forms.DataGridViewCheckBoxColumn DeleteCheck;
     }
 }
