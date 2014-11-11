5c2dfc4d84c8ffef7d7dd5b7182fd8b95175a635
Maximiliano Felice <maximilianofelice@gmail.com>
2014-11-11 13:45:48 -0200

    Added Control Extensions for visibility and access

--- src/FrbaHotel/FrbaHotel/Form1.cs
+++ src/FrbaHotel/FrbaHotel/Form1.cs
@@ -7,7 +7,6 @@ using System.Linq;
 using System.Text;
 using System.Windows.Forms;
 using HotelModel;
-using HotelModel.User_Permissions.ControlManager;
 
 namespace FrbaHotel
 {
---
+++ src/FrbaHotel/HotelModel/User Permissions/ControlExtensions.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace HotelModel.User_Permissions
+{
+    public static class ControlExtensions
+    {
+        public static void HandleVisibility(this Control ctrl)
+        {
+            PermissionManager.addVisibleControl(ctrl);
+        }
+
+        public static void HandleAccess(this Control ctrl)
+        {
+            PermissionManager.addAccessibleControl(ctrl);
+        }
+    }
+}
